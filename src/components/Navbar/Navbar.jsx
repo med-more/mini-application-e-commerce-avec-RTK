cdd00c665f452d1895880cd4c2878f183e5e980a
med-more <166568621+med-more@users.noreply.github.com>
2025-12-16 19:19:13 +0000

    add navbar navigation links and branding

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -6,7 +6,25 @@ const Navbar = ({ onCartClick }) => {
   const cartItems = useSelector((state) => state.cart.items)
   const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0)
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-container">
+        <Link to="/" className="navbar-logo">
+          TechShop      
+        </Link>
+
+        <div className="navbar-links">
+          <Link to="/" className="navbar-link">
+            Accueil
+          </Link>
+          <Link to="/catalogue" className="navbar-link">
+            Catalogue
+          </Link>
+          <Link to="/wishlist" className="navbar-link">
+            Favoris
+          </Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 
