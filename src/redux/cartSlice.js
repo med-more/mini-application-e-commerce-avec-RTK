7aa5bcba57fd243c87796b25ad8d96b9d616da92
med-more <166568621+med-more@users.noreply.github.com>
2025-12-16 14:50:05 +0000

    add cart slice with addToCart reducer

---
+++ src/redux/cartSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const cartSlice = createSlice({
+  name: "cart",
+  initialState:{
+    items: [],
+  },
+  reducers: {
+    addToCart: (state, action) =>{
+      const product = action.payload
+      const existingItem = state.items.find((item) => item.id === product.id)
+
+      if (existingItem) {
+        existingItem.quantity += 1
+      } else {
+        state.items.push({ ...product, quantity: 1 })
+      }
+    },
+  }
+})
