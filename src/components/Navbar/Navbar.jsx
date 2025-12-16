07294a8adbd4383470ef960a0b8b465b9ce7fb5a
med-more <166568621+med-more@users.noreply.github.com>
2025-12-16 19:12:42 +0000

    add Navbar component with cart items count

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { useSelector } from "react-redux"
+import "./Navbar.css"
 
-const Navbar = () => {
+const Navbar = ({ onCartClick }) => {
+  const cartItems = useSelector((state) => state.cart.items)
+  const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0)
   return (
     <div>Navbar</div>
   )
