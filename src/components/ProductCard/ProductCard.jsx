2174b7803f864a62b48aa6a084d32864a7ffebd5
med-more <166568621+med-more@users.noreply.github.com>
2025-12-17 21:41:07 +0000

    toggle wishlist items in ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -3,11 +3,19 @@ import { addToCart } from "../../redux/cartSlice"
 import { addToWishlist, removeFromWishlist } from "../../redux/wishlistSlice"
 import "./ProductCard.css"
 
-const ProductCard = ({ product }) => {
+const ProductCard = ({ product, isInWishlist }) => {
   const dispatch = useDispatch();
 
   const handleAddToCart = () =>{
     dispatch(addToCart(product))
+  }
+
+  const handleWishlist = () =>{
+    if (isInWishlist) {
+      dispatch(removeFromWishlist(product.id))
+    } else{
+      dispatch(addToWishlist(product))
+    }
   }
   return (
     <div>ProductCard</div>
