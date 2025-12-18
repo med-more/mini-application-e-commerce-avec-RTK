c734454cd1169824735775513d726d76e2eadbdd
med-more <166568621+med-more@users.noreply.github.com>
2025-12-18 00:24:06 +0000

    select top 6 featured products

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import "./Home.css"
 
 const Home = () => {
   const wishlistItems = useSelector((state) => state.wishlist.items)
+
+  const featuredProducts = products.slice(0, 6)
   return (
     <div>Home</div>
   )
