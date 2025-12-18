e31a69676afa5367dc9bede0b78733d58d1dd862
med-more <166568621+med-more@users.noreply.github.com>
2025-12-18 14:29:17 +0000

    add Catalogue page with wishlist state integration

--- src/pages/Catalogue/Catalogue.jsx
+++ src/pages/Catalogue/Catalogue.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import { useSelector } from "react-redux"
+import ProductCard from "../../components/ProductCard"
+import { product } from "../../data/products"
+import "./Catalogue.css"
 
 const Catalogue = () => {
+  const wishlistItems = useSelector((state) => state.wishlist.items)
   return (
     <div>Catalogue</div>
   )
