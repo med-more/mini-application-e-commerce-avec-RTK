8216fe37d153bd9888355e0b4f8adbf38e43bddb
med-more <mbfmz20@gmail.com>
2026-01-05 21:18:14 +0100

    fix panier position

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,14 +2,15 @@ import { Link } from "react-router-dom"
 import { useSelector } from "react-redux"
 import "./Navbar.css"
 
-const Navbar = ({ onCartClick }) => {
+function Navbar({ onCartClick }) {
   const cartItems = useSelector((state) => state.cart.items)
   const totalItems = cartItems.reduce((total, item) => total + item.quantity, 0)
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <Link to="/" className="navbar-logo">
-          TechShop      
+          TechShop
         </Link>
 
         <div className="navbar-links">
@@ -23,14 +24,14 @@ const Navbar = ({ onCartClick }) => {
             Favoris
           </Link>
         </div>
-      </div>
 
-      <button className="cart-button" onClick={onCartClick}>
-        <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
+        <button className="cart-button" onClick={onCartClick}>
+          <svg width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor">
             <path d="M9 2L7 6H3L6 18H18L21 6H17L15 2H9Z" strokeWidth="2" />
-        </svg>
-        {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
-      </button>
+          </svg>
+          {totalItems > 0 && <span className="cart-badge">{totalItems}</span>}
+        </button>
+      </div>
     </nav>
   )
 }
